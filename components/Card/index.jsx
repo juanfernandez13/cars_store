6b43d12f2b631ca50908a959be5008b9a9ea8605
Juan <juanp1310@outlook.com>
2024-07-06 17:56:24 -0300

    feat: conectando back com front

--- components/Card/index.jsx
+++ components/Card/index.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const CardComponent = props => {
-  const {children} = props
+const CardComponent = (props) => {
+  const { car } = props;
   return (
-    <div className='w-4/5 lg:w-full h-72 rounded border-slate-400 border-2'>
-        <a href={`/search/${children}`}>
-        {children}
-        </a>
+    <div className="w-4/5 lg:w-full h-auto rounded border-slate-400 border-2">
+      <a href={`/search/${car.id}`}>
+        <img src={car.images[0]} />
+        <span>
+          {car.automaker} - {car.model}
+        </span>
+        <p>{car.price}</p>
+        <p>{car.year}</p>
+        <p>Anúncio visto {car.views} vezes</p>
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default CardComponent
+export default CardComponent;
--- pages/search/index.jsx
+++ pages/search/index.jsx
@@ -1,14 +1,26 @@
 import { CardComponent, SearchBarComponent } from "@/components";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function SearchView() {
   const [searchValue, setSearchValue] = useState('')
-  
+  const [cars, setCars] = useState([])
+
+  useEffect(() => {
+    getAllCars()
+  }, []
+)
+
+const getAllCars = async () => {
+  const response = await fetch('http://localhost:3000/api/car/')
+  const data = await response.json()
+  console.log(data)
+  setCars(data.data)
+}
   return (
     <div className="w-full min-h-screen h-full">
       <SearchBarComponent />
       <div className="px-4 h-full w-fullgrid-cols-1 md:grid-cols-2 lg:grid-cols-4 grid justify-items-center	gap-8 py-4">
-        {[1,2,1,1,5].map((item, index) => (<CardComponent key={index}>{item}</CardComponent>))}
+        {cars.map((item, index) => (<CardComponent key={index} car={item}/>))}
       </div>
     </div>
   );
