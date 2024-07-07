3226390674cfb53e81f8d3cf6e34f43b2d16c514
Juan <juanp1310@outlook.com>
2024-07-07 17:14:08 -0300

    fix: correção de bugs na navbar e no card

--- components/Card/index.jsx
+++ components/Card/index.jsx
@@ -5,12 +5,12 @@ const CardComponent = (props) => {
   return (
     <div className="w-4/5 lg:w-full h-auto rounded border-slate-400 border-2">
       <a href={`/search/${car.id}`}>
-        <img src={car.images[0]} />
-        <span>
-          {car.automaker} - {car.model}
-        </span>
-        <p>{car.price}</p>
-        <p>{car.year}</p>
+        <img src={car.images[0]} className="h-3/5 w-full object-cover"/>
+        <p>
+          <span className="font-bold">{car.automaker}</span> - {car.model}
+        </p>
+        <p>R$: {car.price}</p>
+        <p>Ano: {car.year}</p>
         <p>Anúncio visto {car.views} vezes</p>
       </a>
     </div>
--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,7 +1,5 @@
 import { People, Search } from "@mui/icons-material";
-import { colors } from "@mui/material";
 import { AppBar, Button } from "@mui/material";
-import Link from "next/link";
 
 const NavbarComponent = () => {
   return (
@@ -11,7 +9,7 @@ const NavbarComponent = () => {
           "py-6 px-4 bg-gradient-to-l from-orange-400 to-orange-800 flex justify-between flex-row"
         }
       >
-        <h1 className={"text-2xl font-bold"}>Cars store</h1>
+        <a href="/"><h1 className={"text-2xl font-bold"}>Cars store</h1></a>
         <div>
           <Button className="lg:mr-4">
             <a href={'/search'} className="gap-4 flex flex-row">
